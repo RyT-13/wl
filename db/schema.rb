0c4e2e093e999cb62edf4db236f1b9ae9a087018
Vasiliy <ryt.13@yandex.ru>
2020-01-06 04:27:43 +0300

    Lists.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_04_220112) do
+ActiveRecord::Schema.define(version: 2020_01_05_213514) do
+
+  create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
